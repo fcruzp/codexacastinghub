b1d9765174a4ade42dd0b36ff27fd14615a0d15d
fcruzp <fcruzpyt2@gmail.com>
2025-05-29 21:50:33 -0400

    version 0.1

--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -6,7 +6,6 @@ import { Button } from "../components/ui/button";
 import { Input } from "../components/ui/input";
 import { Label } from "../components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../components/ui/select";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "../components/ui/tabs";
 import { Badge } from "../components/ui/badge";
 import { Calendar, MapPin, Building2, Clock, Lock, ArrowRight, Home } from "lucide-react";
 
